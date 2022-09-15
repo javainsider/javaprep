f7440f5035401626afcd4987bbbdc97498c3e724
Rudra Garnaik <rngarnaik@users.noreply.github.com>
2022-09-15 16:16:28 -0400

    Solution to Make Valid Parentheses
    Make Valid Parentheses. Find how many invalid parenthesis are present in the the provided string value.

---
+++ src/main/java/com/javainsider/javaprep/medium/MakeValidParentheses.java
@@ -0,0 +1,33 @@
+package com.javainsider.javaprep.medium;
+
+public class MakeValidParentheses {
+
+    public static void main(String[] args) {
+        String input1 = "())"; // Ans 1
+        String input2 = ")))"; // Ans 3
+        String input3 = " ())))"; // Ans 3
+
+
+        System.out.println(" Answer to input 1: "+ minAddToMakeValid(input1));
+        System.out.println(" Answer to input 2: "+ minAddToMakeValid(input2));
+        System.out.println(" Answer to input 3: "+ minAddToMakeValid(input3));
+    }
+
+
+    public static int minAddToMakeValid(String s) {
+        int cnt =0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '('){
+                cnt ++;
+            }
+            if (c == ')') {
+                cnt --;
+            }
+        }
+        if (cnt < 0 ){
+            cnt = - cnt;
+        }
+        return cnt;
+    }
+}
